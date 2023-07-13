3f6923ee4934e02d3e68f554d79f22525aed596a
isma <IsmaFont@github.com>
2023-07-13 04:01:35 +0000

    added failsafe stuff

--- startConfig.sh
+++ startConfig.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 # Countdown from input to 0 with 1s delay
+getVars () {
+	printf "Username: " ; read inUser
+
+}
+
 countDown () {
 	for i in $(seq 0 $* | sort -nr)
 	do
@@ -20,28 +25,31 @@ printSpacer () {
 # Update repos and upgrade base packages
 updatePkg () {
 	printSpacer "Updating repo and upgrading packages..."
-	apt update -y; apt upgrade -y
+	sudo apt update -y; sudo apt upgrade -y
 	printSpacer "Done updating repo and upgrading packages."
 }
 
 # Install new internal packages
 installIntPkg () {
 	printSpacer "Installing new internal packages..."
-	apt install $(cat ./aptPackages.list)
+	sudo apt install $(cat ./aptPackages.list)
 	printSpacer "Done installing new internal packages."
 }
 
 modBashrc () {
 	printSpacer "Modifying .bashrc file..."
-	cat bashrc.txt >> fkbashr
-	sdiff bashrc.txt fkbashr
-	source "$HOME/.bashrc"
+	cat bashrc.txt >> /home/$inUser/.bashrc
+	sdiff bashrc.txt /home/$inUser/.bashrc
+	source "/home/$inUser/.bashrc"
 	printSpacer "Done modifying .bashrc file."
 }
 
 # Main function, used to keep everything clean and tidy
 main () {
 	cat ./cmotd.txt
+	if [[ "$USER" == "root" ]]; then echo run this as your normal user ; exit 2; fi
+	getVars
+	echo $inUser
 	countDown 5
 	updatePkg
 	installIntPkg
